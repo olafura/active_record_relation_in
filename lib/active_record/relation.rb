fe7eee2b7d5292239544b5163d413c759421beb2
Olafur Arason <olafura@olafura.com>
2023-08-07 18:09:32 +0200

    wip

---
+++ lib/active_record/relation.rb
@@ -0,0 +1,18 @@
+module ActiveRecord
+  class Relation
+    def to_relation_in
+      # attr_reader :table, :klass, :loaded, :predicate_builder
+      # def initialize(klass, table: klass.arel_table, predicate_builder: klass.predicate_builder, values: {})
+      #   @klass  = klass
+      #   @table  = table
+      #   @values = values
+      #   @loaded = false
+      #   @predicate_builder = predicate_builder
+      #   @delegate_to_klass = false
+      #   @future_result = nil
+      #   @records = nil
+      # end
+      ActiveRecordRelationIn::Relation.new(@klass, table: @table, predicate_builder: @predicate_builder, values: @values)
+    end
+  end
+end
