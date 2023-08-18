cb223e94bd8b03aad1dd775223aa6abf7e1f6d46
Olafur Arason <olafura@olafura.com>
2023-08-18 17:59:21 +0200

    wip

--- spec/active_record/relation_spec.rb
+++ spec/active_record/relation_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe ActiveRecord::Relation do
     let(:post) { Post.new(user_id: user.id) }
 
     it 'has method' do
-      ActiveRecord::Relation.method_defined? :relation_in
-      Post.for_users(user.id).method_defined? :relation_in
+      expect(ActiveRecord::Relation.method_defined? :relation_in).to be true
+      expect(Post.for_users(user.id).method_defined? :relation_in).to be true
       byebug
       new_scope = Post.for_users(user.id).relation_in
       puts new_scope.to_sql
